40acf50a283b13a81c29c41575ea13c6755c9f16
khuatlong0902 <khuatlong0902@gmail.com>
2023-12-13 14:15:09 +0700

    Ui

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,21 @@
 import SelectOrganization from './Authen/SelectOrganization';
 import SignIn from './Authen/SignIn';
+import Home from './Home';
+import ForgotPassword from './Authen/ForgotPassword';
+import SuccessChanged from './Authen/SuccessChanged';
+import ListProduct from './Product/ProductList/ListProduct';
+import SearchProduct from './Product/ProductList/SearchProduct';
+import ProductDetail from './Product/ProductDetail/ProductDetail';
+import ImageView from './Product/ProductDetail/ImageView';
 
-export {SelectOrganization, SignIn};
+export {
+    SelectOrganization,
+    SignIn,
+    Home,
+    SuccessChanged,
+    ForgotPassword,
+    ListProduct,
+    SearchProduct,
+    ProductDetail,
+    ImageView,
+};
