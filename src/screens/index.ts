ffc06e039659e1f7ad75273999acd4469e289503
khuatlong0902 <khuatlong0902@gmail.com>
2023-12-13 14:15:09 +0700

    Ui

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -39,3 +39,22 @@ export {
   AddingNewCustomer,
   ListVisit,
 };
+import Home from './Home';
+import ForgotPassword from './Authen/ForgotPassword';
+import SuccessChanged from './Authen/SuccessChanged';
+import ListProduct from './Product/ProductList/ListProduct';
+import SearchProduct from './Product/ProductList/SearchProduct';
+import ProductDetail from './Product/ProductDetail/ProductDetail';
+import ImageView from './Product/ProductDetail/ImageView';
+
+export {
+    SelectOrganization,
+    SignIn,
+    Home,
+    SuccessChanged,
+    ForgotPassword,
+    ListProduct,
+    SearchProduct,
+    ProductDetail,
+    ImageView,
+};
