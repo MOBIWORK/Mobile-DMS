87e17acca4e0b67d2ac9410edbdb4b16b6542245
vanhieuu99 <vanhieuu99@gmail.com>
2024-01-15 22:54:20 +0700

    feat: fix saga

--- src/saga/app-saga/index.ts
+++ src/saga/app-saga/index.ts
@@ -5,6 +5,6 @@ import { takeLatest } from '../../utils/typed-redux-saga';
 import * as Saga from './saga';
 export function* appSaga() {
     yield* takeLatest(appActions.onLoadApp.type,Saga.onLoadAppModeAndTheme);
-    yield* takeLatest  ( appActions.onCheckIn.type,Saga.onCheckInData)
+    yield* takeLatest(appActions.onCheckIn.type,Saga.onCheckInData)
     yield* takeLatest(appActions.onGetLost.type, Saga.onLost);
 }
