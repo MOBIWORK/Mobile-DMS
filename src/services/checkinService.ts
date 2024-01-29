978c4ffa227e0b41fc895df891da7e97ca238c2f
khuatlong0902 <khuatlong0902@gmail.com>
2024-01-29 09:37:22 +0700

    aa

--- src/services/checkinService.ts
+++ src/services/checkinService.ts
@@ -1,5 +1,5 @@
-import { createApi } from '../api';
-import { ApiConstant } from '../const';
+import {createApi} from '../api';
+import {ApiConstant} from '../const';
 
 export type IUpdateAddress = {
   customer: string;
@@ -12,23 +12,5 @@ export type IUpdateAddress = {
   country: 'Việt Nam';
 };
 
-
-export interface checkinItemProduct {
-  item_code: string
-  item_unit: string
-  quantity?: number
-  exp_time: number
-}
-
-export type POST_DATA = {
-  customer_code: string
-  customer_id: string
-  customer_name: string
-  customer_address: string
-  inventory_items: checkinItemProduct[]
-}
-
-
-export const checkinInventory = (data: POST_DATA) => createApi().post(ApiConstant.POST_CHECKIN_INVENTORY, data);
 export const updateCustomerAddress = (data: IUpdateAddress) =>
   createApi().patch(ApiConstant.UPDATE_CUSTOMER_ADDRESS, data);
