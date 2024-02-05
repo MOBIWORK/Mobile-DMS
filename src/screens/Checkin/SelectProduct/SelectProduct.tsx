5e2061b00e4acf7286023d14e4083d9f8137b9bf
khuatlong0902 <khuatlong0902@gmail.com>
2024-02-05 15:10:48 +0700

    merge deveop

--- src/screens/Checkin/SelectProduct/SelectProduct.tsx
+++ src/screens/Checkin/SelectProduct/SelectProduct.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { MainLayout } from '../../../layouts'
-import { AppBottomSheet, AppButton, AppContainer, AppHeader, AppIcons, AppInput } from '../../../components/common'
+import { AppBottomSheet, AppButton, AppCheckBox, AppHeader, AppIcons, AppInput } from '../../../components/common'
 import { ApiConstant, AppConstant } from '../../../const'
-import { useNavigation, useTheme } from '@react-navigation/native'
-import { Text, TextInput as Input, TextStyle, View, ViewStyle, Pressable,TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Text, TextInput as Input, TextStyle, View, ViewStyle, TouchableOpacity, FlatList } from 'react-native'
 import { TextInput } from 'react-native-paper'
 
 import { StyleSheet } from 'react-native'
 import { Searchbar } from 'react-native-paper'
 import { ImageAssets } from '../../../assets'
-import { Checkbox } from 'react-native-paper'
 import { ICON_TYPE } from '../../../const/app.const'
 import BottomSheet from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheet/BottomSheet'
 import FilterListComponent, { IFilterType } from '../../../components/common/FilterListComponent'
@@ -21,6 +20,7 @@ import { IProduct, KeyAbleProps } from '../../../models/types'
 import { ProductService } from '../../../services'
 import { useTranslation } from 'react-i18next'
 import { CommonUtils } from '../../../utils'
+import { AppTheme, useTheme } from '../../../layouts/theme'
 
 const initFilterValue = {
     label: "",
@@ -36,10 +36,12 @@ const SelectProducts = () => {
     const bottomSheetRef = useRef<BottomSheet>(null);
     const bottomSheetRefData = useRef<BottomSheet>(null);
     const snapPoints = useMemo(() => ['85%'], []);
-
+    const styles = createStyles(useTheme())
     const [dataCategoryProduct, setdDtaCategoryProduct] = useState<IFilterType[]>([]);
     const [dataBrandProduct, setDataBrandProduct] = useState<IFilterType[]>([]);
     const [dataIndustry, setDataIndustry] = useState<IFilterType[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const [pageSize, setPageSize] = useState<number>(20);
     const { totalItem, data: products } = useSelector(state => state.product);
     const [data, setData] = useState<IProduct[]>([]);
     const [dataFilter, setDataFilter] = useState<IFilterType[]>([]);
@@ -92,20 +94,18 @@ const SelectProducts = () => {
     const renderUiItem = (item: IProduct) => {
         return (
             <View style={[styles.itemProduct, { backgroundColor: item.isSelected ? "rgba(196, 22, 28, 0.08)" : colors.bg_default }]}>
-                <View style={[styles.flex as any, { alignItems: "flex-start" }]}>
-                    <View style={{ paddingTop: 12, marginRight: 4 }}>
-                        <Checkbox
-                            color={colors.primary}
-                            onPress={() => onSelectProduct(item.item_code, item.isSelected ? item.isSelected : false)}
-                            uncheckedColor={colors.text_secondary}
-                            status={item.isSelected ? 'checked' : 'unchecked'}
+                <View style={[styles.flex as any, { alignItems: "flex-start", columnGap: 6 }]}>
+                    <View style={{ paddingTop: 12 }}>
+                        <AppCheckBox
+                            status={item.isSelected ? true : false}
+                            onChangeValue={() => onSelectProduct(item.item_code, item.isSelected ? item.isSelected : false)}
                         />
                     </View>
 
                     <View style={{ flex: 1 }}>
 
-                        <View style={[styles.flex as any, styles.itemRowIf, styles.itemRowIf, { borderColor: colors.border }]}>
-                            <Text style={[styles.labelIfPrd as TextStyle, { color: colors.text_disable }]}>{getLabel("productCode")}</Text>
+                        <View style={[styles.flex as any, styles.itemRowIf]}>
+                            <Text style={[styles.labelIfPrd]}>{getLabel("productCode")}</Text>
                             <View style={[styles.flex as any]}>
                                 <AppIcons
                                     iconType={ICON_TYPE.IonIcon}
@@ -117,11 +117,11 @@ const SelectProducts = () => {
                             </View>
                         </View>
 
-                        <View style={[styles.flex as any, styles.itemRowIf, { borderColor: colors.border }]}>
-                            <Text style={[styles.labelIfPrd as TextStyle, { color: colors.text_disable }]}>{getLabel("unt")}</Text>
-                            <TouchableOpacity activeOpacity={0.6} onPress={() => openBottomSheetDataFilter("unit",item)}>
-                                <View style={[styles.flex as any, { borderRadius: 8, borderWidth: 1, borderColor: colors.border, paddingVertical: 8 }]}>
-                                    <Text style={[styles.filter as TextStyle, { color: colors.text_primary, marginHorizontal: 20 }]}>{item.stock_uom}</Text>
+                        <View style={[styles.flex as any, styles.itemRowIf]}>
+                            <Text style={[styles.labelIfPrd]}>{getLabel("unt")}</Text>
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => openBottomSheetDataFilter("unit", item)}>
+                                <View style={[styles.flex as any,styles.containerUnit]}>
+                                    <Text style={[styles.filter , {marginHorizontal: 20 }]}>{item.stock_uom}</Text>
                                     <AppIcons
                                         iconType={ICON_TYPE.Feather}
                                         name="chevron-down"
@@ -132,11 +132,11 @@ const SelectProducts = () => {
                             </TouchableOpacity>
                         </View>
 
-                        <View style={[styles.flex as any, styles.itemRowIf, { borderColor: colors.border,paddingVertical : 4}]}>
-                            <Text style={[styles.labelIfPrd as TextStyle, { color: colors.text_disable }]}>{getLabel("quantity")}</Text>
+                        <View style={[styles.flex as any, styles.itemRowIf, {paddingVertical: 4 }]}>
+                            <Text style={[styles.labelIfPrd]}>{getLabel("quantity")}</Text>
                             <View style={[styles.flex as any]}>
                                 <TouchableOpacity
-                                    style={{ paddingHorizontal: 10}}
+                                    style={{ paddingHorizontal: 10 }}
                                     onPress={() => onChangeQuantityProduct(item.item_code, item.quantity && item.quantity > item.min_order_qty ? item.quantity - 1 : item.min_order_qty)}
                                 >
                                     <AppIcons
@@ -155,9 +155,8 @@ const SelectProducts = () => {
                                 />
 
                                 <TouchableOpacity
-                                    style={{ paddingHorizontal: 10}}
+                                    style={{ paddingHorizontal: 10 }}
                                     onPress={() => onChangeQuantityProduct(item.item_code, item.quantity ? item.quantity + 1 : 2)}
-
                                 >
                                     <AppIcons
                                         iconType={ICON_TYPE.IonIcon}
@@ -169,16 +168,18 @@ const SelectProducts = () => {
                             </View>
                         </View>
 
-                        <View style={[styles.flex as any, styles.itemRowIf, { borderColor: colors.border}]}>
-                            <Text style={[styles.labelIfPrd as TextStyle, { color: colors.text_disable }]}>{getLabel("unitPrice")}</Text>
+                        <View style={[styles.flex as any, styles.itemRowIf]}>
+                            <Text style={[styles.labelIfPrd]}>{getLabel("unitPrice")}</Text>
                             <Text style={[styles.labelIfPrd as TextStyle, { color: colors.text_primary, marginLeft: 4 }]}>{CommonUtils.formatCash(item.price.toString())}</Text>
                         </View>
 
-                        <View style={[styles.flex as any, styles.itemRowIf, { borderColor: colors.border,borderBottomWidth : 0  }]}>
-                            <Text style={[styles.labelIfPrd as TextStyle, { color: colors.text_disable }]}>{getLabel("expired")}</Text>
+                        <View style={[styles.flex as any, styles.itemRowIf, {borderBottomWidth: 0 }]}>
+                            <Text style={[styles.labelIfPrd]}>{getLabel("expired")}</Text>
                             <TouchableOpacity activeOpacity={0.6}>
-                                <View style={[styles.flex as any, { borderRadius: 8, borderWidth: 1, borderColor: colors.border, paddingVertical: 8, paddingHorizontal: 5 }]}>
-                                    <Text style={[styles.filter as TextStyle, { color: colors.text_primary, marginHorizontal: 5 }]}>{CommonUtils.convertDate(item.end_of_life)}</Text>
+                                <View style={[styles.flex as any, styles.calenderIcon]}>
+                                    <Text style={[styles.filter,{marginHorizontal: 5 }]}>
+                                        {CommonUtils.convertDate(item.end_of_life)}
+                                    </Text>
                                     <AppIcons
                                         iconType={ICON_TYPE.MaterialCommunity}
                                         name="calendar-month-outline"
@@ -332,7 +333,7 @@ const SelectProducts = () => {
             case 'unit': {
                 const newProducts = data.map(item1 => {
                     let priceUom = item1.details.find(item2 => item2.uom === item.label);
-                    return item1.item_code === item.value ? ({ ...item1, stock_uom: item.label ,price : priceUom ? priceUom.price_list_rate : 0 }) : item1
+                    return item1.item_code === item.value ? ({ ...item1, stock_uom: item.label, price: priceUom ? priceUom.price_list_rate : 0 }) : item1
                 });
                 setData(newProducts);
                 break;
@@ -348,18 +349,18 @@ const SelectProducts = () => {
 
     const onSelectProduct = (id: string, isSelected: boolean) => {
         const newData = data.map(item => {
-            return item.item_code === id ? { ...item, isSelected: !isSelected} : item
+            return item.item_code === id ? { ...item, isSelected: !isSelected } : item
         });
         setData(newData);
     }
-    
-    const isSelected = useMemo(()=>{
+
+    const isSelected = useMemo(() => {
         const dataSelect = data.filter(item => item.isSelected);
         return dataSelect.length > 0 ? false : true
-    },[data])
+    }, [data])
 
-    const onSelectAllProduct = ()=>{
-        const newData = data.map(item => ({...item , isSelected : isSelected}));
+    const onSelectAllProduct = () => {
+        const newData = data.map(item => ({ ...item, isSelected: isSelected }));
         setData(newData);
     }
 
@@ -434,8 +435,8 @@ const SelectProducts = () => {
     useEffect(() => {
         const newDaata = products.map(item => {
             let priceUom = item.details.find(item2 => item2.uom === item.stock_uom);
-            let neItem = {...item,price : priceUom ? priceUom.price_list_rate : 0}
-            return item.min_order_qty === 0 ? {...neItem , quantity : 1 , discount : 0} : {...neItem , quantity : item.min_order_qty ,discount :0}
+            let neItem = { ...item, price: priceUom ? priceUom.price_list_rate : 0 }
+            return item.min_order_qty === 0 ? { ...neItem, quantity: 1, discount: 0 } : { ...neItem, quantity: item.min_order_qty, discount: 0 }
         })
         setData(newDaata)
     }, [products])
@@ -444,14 +445,16 @@ const SelectProducts = () => {
         dispatch(productActions.onGetData({
             item_group: filterProduct.group,
             brand: filterProduct.brand,
-            industry: filterProduct.industry
+            industry: filterProduct.industry,
+            page: page,
+            page_size: pageSize,
         }))
-    }, [filterProduct.brand, filterProduct.group, filterProduct.industry])
+    }, [filterProduct.brand, filterProduct.group, filterProduct.industry, page, pageSize])
 
     return (
         <>
-            <MainLayout style={{ backgroundColor: colors.bg_neutral, paddingHorizontal: 0 }}>
-                <View style={{ backgroundColor: colors.bg_default, paddingHorizontal: 16, paddingBottom: 16 }}>
+            <MainLayout style={styles.layout}>
+                <View style={styles.container}>
                     <AppHeader
                         label={getLabel("product")}
                         onBack={() => navigation.goBack()}
@@ -464,20 +467,13 @@ const SelectProducts = () => {
                             />
                         }
                         rightButton={
-                            <TouchableOpacity
-                                onPress={onSubmitProductSelect}
-                            >
-                                <Text style={[styles.headerAction as any, { color: colors.action }]}>{getLabel("continue")}</Text>
+                            <TouchableOpacity onPress={onSubmitProductSelect}>
+                                <Text style={[styles.headerAction]}>{getLabel("continue")}</Text>
                             </TouchableOpacity>}
                     />
                     <View style={[styles.flex as any, { marginTop: 16 }]}>
                         <Searchbar
-                            style={{
-                                backgroundColor: colors.bg_neutral,
-                                borderRadius: 10,
-                                height: 50,
-                                flex: 1
-                            }}
+                            style={styles.searchStyle}
                             placeholder={getLabel("searchProduct")}
                             placeholderTextColor={colors.text_disable}
                             icon={ImageAssets.SearchIcon}
@@ -499,26 +495,25 @@ const SelectProducts = () => {
                     </View>
 
                 </View>
-                <View style={[styles.flex as any, { justifyContent: "space-between", marginTop: 24, paddingHorizontal: 16, marginBottom: 16 }]}>
+                <View style={[styles.flex as any,styles.titleContent]}>
                     <TouchableOpacity onPress={() => onSelectAllProduct()}>
-                        <Text style={[styles.action as any, { color: colors.action }]}>
-                            {isSelected ?  getLabel("selectAll") : getLabel("deselectAll")}
+                        <Text style={[styles.action]}>
+                            {isSelected ? getLabel("selectAll") : getLabel("deselectAll")}
                         </Text>
                     </TouchableOpacity>
                     <Text style={[styles.filter as any, { color: colors.text_secondary }]}>{getLabel("total")} : <Text style={{ color: colors.text_primary, fontWeight: "500" }}>{totalItem} {` `}</Text>
                         {getLabel("product").toLowerCase()}
                     </Text>
                 </View>
-                <AppContainer>
-                    <View style={{ paddingHorizontal: 16, rowGap: 16 }}>
-                        {data.map(item => (
-                            <Pressable key={item.item_code}>
-                                {renderUiItem(item)}
-                            </Pressable>
-                        ))}
-
-                    </View>
-                </AppContainer>
+                <View style={{ paddingHorizontal: 16}}>
+                    <FlatList
+                        data={data}
+                        renderItem={({ item }) => <View>{renderUiItem(item)}</View> }
+                        onEndReached={() => setPage(page + 1)}
+                        contentContainerStyle={{ rowGap: 16 }}
+                        showsVerticalScrollIndicator={false}
+                    />
+                </View>
             </MainLayout>
             <AppBottomSheet bottomSheetRef={bottomSheetRef} snapPointsCustom={snapPoints}>
                 {bottomSheetFilter()}
@@ -542,12 +537,22 @@ const SelectProducts = () => {
 
 export default SelectProducts;
 
-const styles = StyleSheet.create({
+const createStyles = (theme : AppTheme)=> StyleSheet.create({
+    layout : {
+        backgroundColor: theme.colors.bg_neutral,
+        paddingHorizontal: 0
+    }as ViewStyle,
+    container :{
+        backgroundColor: theme.colors.bg_default,
+        paddingHorizontal: 16,
+        paddingBottom: 16
+    } as ViewStyle,
     headerAction: {
         fontSize: 16,
         lineHeight: 24,
-        fontWeight: "500"
-    },
+        fontWeight: "500",
+        color : theme.colors.action
+    } as TextStyle,
     flex: {
         flexDirection: "row",
         alignItems: "center"
@@ -555,18 +560,21 @@ const styles = StyleSheet.create({
     filter: {
         fontSize: 14,
         lineHeight: 21,
-        fontWeight: "400"
-    },
+        fontWeight: "400",
+        color : theme.colors.text_primary
+    } as TextStyle,
     action: {
         fontSize: 14,
         lineHeight: 21,
-        fontWeight: "500"
-    },
+        fontWeight: "500",
+        color : theme.colors.action
+    } as TextStyle,
     labelIfPrd: {
         fontSize: 16,
         lineHeight: 24,
-        fontWeight: "400"
-    },
+        fontWeight: "400",
+        color : theme.colors.text_disable
+    } as TextStyle,
     itemProduct: {
         paddingHorizontal: 16,
         paddingVertical: 12,
@@ -575,8 +583,9 @@ const styles = StyleSheet.create({
     itemRowIf: {
         paddingVertical: 12,
         justifyContent: "space-between",
-        borderBottomWidth: 1
-    },
+        borderBottomWidth: 1,
+        borderColor : theme.colors.border
+    }as ViewStyle,
     containerButton: {
         justifyContent: 'space-between',
         flexDirection: 'row',
@@ -585,5 +594,30 @@ const styles = StyleSheet.create({
         bottom: 0,
         width: '100%',
         alignSelf: 'center',
-    } as ViewStyle
+    } as ViewStyle,
+    titleContent :{
+        justifyContent: "space-between",
+        marginTop: 24,
+        paddingHorizontal: 16,
+        marginBottom: 16
+    }as ViewStyle,
+    searchStyle :{
+        backgroundColor: theme.colors.bg_neutral,
+        borderRadius: 10,
+        height: 50,
+        flex: 1
+    }as ViewStyle,
+    calenderIcon : {
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: theme.colors.border,
+        paddingVertical: 8,
+        paddingHorizontal: 5 
+    }as ViewStyle,
+    containerUnit : {
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: theme.colors.border,
+        paddingVertical: 8
+    }as ViewStyle
 })
