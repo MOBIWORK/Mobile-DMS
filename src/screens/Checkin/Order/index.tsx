f63acd987c353ff3ea16fdb4e45dfc0e710f39ed
khuatlong0902 <khuatlong0902@gmail.com>
2024-03-05 09:25:46 +0700

    a

--- src/screens/Checkin/Order/index.tsx
+++ src/screens/Checkin/Order/index.tsx
@@ -209,7 +209,7 @@ const CheckinOrder = () => {
                                 </View>
                                 <View style={[styles.flexSpace]}>
                                     <Text style={[styles.labelDetail]}>{getLabel("totalPrice")} </Text>
-                                    <Text style={[styles.totalPrice]}>{CommonUtils.formatCash(data.grand_total.toString())}</Text>
+                                    <Text style={[styles.totalPrice]}>{CommonUtils.formatCash(data.grand_total?.toString())}</Text>
                                 </View>
                             </View>
                         </View>
