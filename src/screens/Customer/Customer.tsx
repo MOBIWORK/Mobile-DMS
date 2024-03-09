4cddd60cd0d6c530601dd18587a0fe55881f6722
SonDN-DDS <ngocsondds@gmail.com>
2024-03-09 09:53:55 +0700

    fix

--- src/screens/Customer/Customer.tsx
+++ src/screens/Customer/Customer.tsx
@@ -8,17 +8,17 @@ import {
   ImageStyle,
   ActivityIndicator,
 } from 'react-native';
-import { useTranslation } from 'react-i18next';
-import React, { useRef, useMemo, useCallback, useEffect } from 'react';
-import { TextInput } from 'react-native-paper';
-import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
+import {useTranslation} from 'react-i18next';
+import React, {useRef, useMemo, useCallback, useEffect} from 'react';
+import {TextInput} from 'react-native-paper';
+import {BottomSheetMethods} from '@gorhom/bottom-sheet/lib/typescript/types';
 
-import { AppConstant, ScreenConstant } from '../../const';
-import { Colors } from '../../assets';
+import {AppConstant, ScreenConstant} from '../../const';
+import {Colors} from '../../assets';
 import MainLayout from '../../layouts/MainLayout';
 import AppImage from '../../components/common/AppImage';
 import FilterHandle from './components/FilterHandle';
-import { listFilter } from './components/data';
+import {listFilter} from './components/data';
 import ListCard from './components/ListCard';
 import {
   AppBottomSheet,
@@ -29,9 +29,9 @@ import {
   Block,
 } from '../../components/common';
 import ListFilter from './components/ListFilter';
-import { NavigationProp } from '../../navigation';
-import { AppTheme, useTheme } from '../../layouts/theme';
-import { useNavigation } from '@react-navigation/native';
+import {NavigationProp} from '../../navigation';
+import {AppTheme, useTheme} from '../../layouts/theme';
+import {useNavigation} from '@react-navigation/native';
 import {
   calculateDistance,
   handleBackgroundLocation,
@@ -50,7 +50,7 @@ export type IValueType = {
 };
 
 const Customer = () => {
-  const { t: getLabel } = useTranslation();
+  const {t: getLabel} = useTranslation();
   const theme = useTheme();
   const styles = rootStyles(theme);
   const dispatch = useDispatch();
@@ -286,7 +286,7 @@ const Customer = () => {
             label={getLabel('groupCustomer')}
             value={valueFilter.customerGroupType}
             editable={false}
-            styles={{ marginBottom: 24 }}
+            styles={{marginBottom: 24}}
             onPress={() => {
               setTypeFilter(AppConstant.CustomerFilterType.nhom_khach_hang);
               filterRef.current?.snapToIndex(0);
@@ -294,7 +294,7 @@ const Customer = () => {
             rightIcon={
               <TextInput.Icon
                 icon={'chevron-down'}
-                style={{ width: 24, height: 24 }}
+                style={{width: 24, height: 24}}
                 color={theme.colors.text_secondary}
               />
             }
@@ -303,7 +303,7 @@ const Customer = () => {
             label={getLabel('customerType')}
             value={valueFilter.customerType}
             editable={false}
-            styles={{ marginBottom: 24 }}
+            styles={{marginBottom: 24}}
             onPress={() => {
               setTypeFilter(AppConstant.CustomerFilterType.loai_khach_hang);
               filterRef.current?.snapToIndex(0);
@@ -311,7 +311,7 @@ const Customer = () => {
             rightIcon={
               <TextInput.Icon
                 icon={'chevron-down'}
-                style={{ width: 24, height: 24 }}
+                style={{width: 24, height: 24}}
                 color={theme.colors.text_secondary}
               />
             }
@@ -327,7 +327,7 @@ const Customer = () => {
             rightIcon={
               <TextInput.Icon
                 icon={'chevron-down'}
-                style={{ width: 24, height: 24 }}
+                style={{width: 24, height: 24}}
                 color={theme.colors.text_secondary}
               />
             }
@@ -355,7 +355,7 @@ const Customer = () => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <Block block>
       <MainLayout style={styles.backgroundRoot}>
         <View style={styles.rootHeader}>
           <Text style={styles.labelStyle}>{getLabel('customer')}</Text>
@@ -384,8 +384,6 @@ const Customer = () => {
           listFooter={listFooter}
           onLoadData={onEndReachedThreshold}
         />
-
-
       </MainLayout>
 
       <AppBottomSheet
@@ -395,10 +393,10 @@ const Customer = () => {
           if (index != undefined && toIndex != undefined) {
             let cal = index - toIndex;
             if (cal > 0) {
-              setShow(prev => ({ ...prev, firstModal: false }));
+              setShow(prev => ({...prev, firstModal: false}));
               //  dispatch(appActions.setShowModal(false))
             } else {
-              setShow(prev => ({ ...prev, firstModal: true }));
+              setShow(prev => ({...prev, firstModal: true}));
               //  dispatch(appActions.setShowModal(true))
             }
           }
@@ -445,10 +443,10 @@ const Customer = () => {
           if (index != undefined && toIndex != undefined) {
             let cal = index - toIndex;
             if (cal > 0) {
-              setShow(prev => ({ ...prev, secondModal: false }));
+              setShow(prev => ({...prev, secondModal: false}));
               //  dispatch(appActions.setShowModal(false))
             } else {
-              setShow(prev => ({ ...prev, secondModal: true }));
+              setShow(prev => ({...prev, secondModal: true}));
               //  dispatch(appActions.setShowModal(true))
             }
           }
@@ -468,15 +466,16 @@ const Customer = () => {
           valueFilter={valueFilter}
         />
       </AppBottomSheet>
-      <AppFAB
-        icon="plus"
-        style={styles.fab}
-        color="white"
-        customIconSize={32}
-        mode="flat"
-        visible={!(show.firstModal || show.secondModal)}
+      <TouchableOpacity
         onPress={() => navigation.navigate(ScreenConstant.ADDING_NEW_CUSTOMER)}
-      />
+        style={[styles.fab]}>
+        <AppIcons
+          iconType="IonIcon"
+          name="add-outline"
+          size={40}
+          color={theme.colors.white}
+        />
+      </TouchableOpacity>
     </Block>
   );
 };
@@ -520,7 +519,7 @@ const rootStyles = (theme: AppTheme) =>
       width: 200,
       flex: 1,
     } as ViewStyle,
-    labelContentStyle: { alignSelf: 'flex-end' } as ViewStyle,
+    labelContentStyle: {alignSelf: 'flex-end'} as ViewStyle,
     rootHeader: {
       flexDirection: 'row',
       justifyContent: 'space-between',
@@ -554,12 +553,12 @@ const rootStyles = (theme: AppTheme) =>
       marginBottom: 5,
     } as ViewStyle,
     itemText: (text: string, value: string) =>
-    ({
-      fontSize: 16,
-      fontWeight: text === value ? '600' : '400',
-      lineHeight: 21,
-      marginBottom: 16,
-    } as TextStyle),
+      ({
+        fontSize: 16,
+        fontWeight: text === value ? '600' : '400',
+        lineHeight: 21,
+        marginBottom: 16,
+      } as TextStyle),
     containListFilter: {
       marginTop: 24,
       flex: 1,
@@ -624,8 +623,8 @@ const rootStyles = (theme: AppTheme) =>
       textAlign: 'center',
     } as TextStyle,
     fab: {
-      width :60,
-      height :60,
+      width: 60,
+      height: 60,
       flex: 1,
       zIndex: 99,
       right: 20,
@@ -634,7 +633,9 @@ const rootStyles = (theme: AppTheme) =>
       backgroundColor: theme.colors.primary,
       borderWidth: 2,
       borderColor: Colors.white,
-      zIndex: 9999999,
+      position: 'absolute',
+      justifyContent: 'center',
+      alignItems: 'center',
     } as ViewStyle,
     backgroundRoot: {
       backgroundColor: theme.colors.bg_neutral,
