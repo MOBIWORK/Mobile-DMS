ac27a69e4f1c3efe868035fc43d832853e91df37
vanhieuu99 <vanhieuu99@gmail.com>
2024-06-19 10:14:20 +0700

    fix list item

--- src/screens/Visit/VisitList/ListVisit.tsx
+++ src/screens/Visit/VisitList/ListVisit.tsx
@@ -851,6 +851,7 @@ const ListVisit = () => {
           };
           setModalAlert(prev => ({ ...prev, status: false }));
           dispatch(appActions.setDataCheckIn(data));
+          dispatch(appActions.onGetSystemConfig());
           if (isDetail) {
             navigate(ScreenConstant.VISIT_DETAIL, { data });
           } else {
@@ -988,6 +989,7 @@ const ListVisit = () => {
         //set CheckIn Time:
         CommonUtils.storage.set(AppConstant.CheckinTime, new Date().getTime());
         //navigation
+        dispatch(appActions.onGetSystemConfig());
         navigate(ScreenConstant.CHECKIN, {
           item: data,
         });
