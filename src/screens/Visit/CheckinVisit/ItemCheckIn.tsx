a3fa24d45985f61b92d7d926918c396a4a978330
SonDN-DDS <ngocsondds@gmail.com>
2024-07-03 11:07:35 +0700

    fix

--- src/screens/Visit/CheckinVisit/ItemCheckIn.tsx
+++ src/screens/Visit/CheckinVisit/ItemCheckIn.tsx
@@ -6,8 +6,7 @@ import {useTheme} from '../../../layouts/theme';
 import isEqual from 'react-fast-compare';
 import {CheckinData} from '../../../services/appService';
 import {navigate} from '../../../navigation/navigation-service';
-import {ErrorBoundary} from 'react-error-boundary';
-import ErrorFallBack from '../../../layouts/ErrorFallBack';
+
 import {useSelector} from '../../../config/function';
 import {shallowEqual} from 'react-redux';
 
@@ -25,68 +24,56 @@ const ItemCheckIn = ({item, navData}: Props) => {
   );
 
   return (
-    <ErrorBoundary fallbackRender={ErrorFallBack}>
-      <Block colorTheme="bg_default">
-        <TouchableOpacity
-          onPress={
-            () =>
-              startTrans(() => {
-                if (listImageToMark && listImageToMark.length > 0) {
-                  navigate(
-                    item?.screenName2 ? item.screenName2 : item.screenName,
-                    {
-                      type: item.type ? item.type : '',
-                      data: navData,
-                      screen: item.screenName2,
-                    },
-                  );
-                } else {
-                  navigate(item.screenName, {
-                    type: item.type ? item.type : '',
-                    data: navData,
-                    screen: item.screenName,
-                  });
-                }
-              })
-          }>
-          <Block
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center">
+    <Block colorTheme="bg_default">
+      <TouchableOpacity
+        onPress={() =>
+          startTrans(() => {
+            if (listImageToMark && listImageToMark.length > 0) {
+              navigate(item?.screenName2 ? item.screenName2 : item.screenName, {
+                type: item.type ? item.type : '',
+                data: navData,
+                screen: item.screenName2,
+              });
+            } else {
+              navigate(item.screenName, {
+                type: item.type ? item.type : '',
+                data: navData,
+                screen: item.screenName,
+              });
+            }
+          })
+        }>
+        <Block
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center">
+          <Block direction="row" alignItems="center" block paddingVertical={8}>
             <Block
-              direction="row"
+              width={36}
+              paddingRight={8}
               alignItems="center"
-              block
-              paddingVertical={8}>
-              <Block
-                width={36}
-                paddingRight={8}
-                alignItems="center"
-                justifyContent="center"
-                height={36}
-                paddingHorizontal={8}
-                colorTheme={item.backgroundColor}
-                borderRadius={8}>
-                <SvgIcon source={item.icon} size={20} color={colors.border} />
-              </Block>
-              <Block direction="row" alignItems="center">
-                <Text fontSize={14} colorTheme="text_primary">
-                  {' '}
-                  {item.name}
-                </Text>
-                {item.isRequire ? <SvgIcon source="Alert" size={16} /> : null}
-              </Block>
+              justifyContent="center"
+              height={36}
+              paddingHorizontal={8}
+              colorTheme={item.backgroundColor}
+              borderRadius={8}>
+              <SvgIcon source={item.icon} size={20} color={colors.border} />
             </Block>
             <Block direction="row" alignItems="center">
-              {item.isDone ? (
-                <SvgIcon source={'CheckCircle'} size={16} />
-              ) : null}
-              <SvgIcon source="arrowRight" size={24} />
+              <Text fontSize={14} colorTheme="text_primary">
+                {' '}
+                {item.name}
+              </Text>
+              {item.isRequire ? <SvgIcon source="Alert" size={16} /> : null}
             </Block>
           </Block>
-        </TouchableOpacity>
-      </Block>
-    </ErrorBoundary>
+          <Block direction="row" alignItems="center">
+            {item.isDone ? <SvgIcon source={'CheckCircle'} size={16} /> : null}
+            <SvgIcon source="arrowRight" size={24} />
+          </Block>
+        </Block>
+      </TouchableOpacity>
+    </Block>
   );
 };
 
