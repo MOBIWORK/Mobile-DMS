79dfb62859898b486910b920a87119d9e6bbb4f0
vanhieuu99 <vanhieuu99@gmail.com>
2024-07-03 11:37:41 +0700

    fix: display distance

--- src/screens/Visit/VisitList/ListVisit.tsx
+++ src/screens/Visit/VisitList/ListVisit.tsx
@@ -892,9 +892,6 @@ const ListVisit = () => {
     [],
   );
 
-
-  
-
   const handleCompareDistance = useCallback(
     (item: VisitListItemType, isDetail: boolean) => {
       let location: LocationProps = JSON.parse(item.customer_location_primary!);
@@ -912,12 +909,13 @@ const ListVisit = () => {
         setTimeout(() => {
           startEffect(() => {
             CommonUtils.getCurrentLocation(curLocation => {
-              let data = calculateDistance(
+              let data =  calculateDistance(
                 curLocation.coords.latitude,
                 curLocation.coords.longitude,
                 location?.lat,
                 location.long,
               );
+              console.log(data,'data distance')
               if (
                 data >
                   (systemConfig.saiso_chophep_kb_vitringoaisaiso +
@@ -952,7 +950,7 @@ const ListVisit = () => {
         });
       }
     },
-    [modalAlert.status, modalUpdateLocation.status,currentSelect.current],
+    [modalAlert.status, modalUpdateLocation.status, currentSelect.current],
   );
 
   const handleBackground = useCallback((item: VisitListItemType) => {
